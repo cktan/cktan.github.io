c0eda9066964f354f8ff0b5e294ad1e0b10f97c6
cktan <cktanx@gmail.com>
2015-05-17 22:02:12 -0700

    new post

---
+++ _posts/2015-05-17-postgres-compare-table.sql
@@ -0,0 +1,15 @@
+---
+layout: post
+title: Comparing Tables in Postgres
+---
+
+How to tell if two tables are exactly the same?
+
+-----
+
+```
+with A as (select hashtext(textin(record_out(a))) as h, count(*) as c from table1 a group by 1)
+, B as (select hashtext(textin(record_out(b))) as h, count(*) as c from table2 b group by 1)
+select * from A full outer join B on (A.h + A.c = B.h + B.c) where A.h is null or B.h is null
+limit 5;
+```
